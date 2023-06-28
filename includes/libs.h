36583e34e48aef17d0d894fc72843c59c3bf71c7
Yuki Ichinose <yichinos@c1r14s7.42tokyo.jp>
2023-06-28 11:29:07 +0900

    iroiro

--- includes/libs.h
+++ includes/libs.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   libs.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yichinos <$yichinos@student.42tokyo.jp>    +#+  +:+       +#+        */
+/*   By: yichinos <yichinos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 16:23:31 by ynishimu          #+#    #+#             */
-/*   Updated: 2023/06/22 21:29:03 by yichinos         ###   ########.fr       */
+/*   Updated: 2023/06/28 11:28:29 by yichinos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBS_H
 # define LIBS_H
 
-# include "../minilibx-linux/mlx.h"
+# include <mlx.h>
 # include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
